a160897745b0cc24751f92a7dce553bfaad13eef
TharlisSampaio <tharlis.sampaio@outlook.com>
2023-04-07 18:23:04 -0500

    adicionando a logica na lista de tarefas

--- To-Do_List/app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ To-Do_List/app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task 2", checked: false},
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
