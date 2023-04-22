8b00ec59c767b49ce6df84491101af4de7857b70
unknown <tharlis.sampaio@outlook.com>
2023-04-22 00:27:18 -0300

    parametros na rota

--- rotas/routes/src/app/pages/sobre/sobre.component.ts
+++ rotas/routes/src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit{
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+    // params é para pegar os parametros da rota no exemplo id e username
+    this.activatedRoute.params.subscribe(
+      res => console.log(res['id'], res['username'], res)
+    )
+
+    // queryParams é para pegar os paramentro passado na url
+    this.activatedRoute.queryParams.subscribe(
+      res => console.log(res['idade'])
+    )
+  }
 
 }
